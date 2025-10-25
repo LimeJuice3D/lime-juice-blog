93772c99827631dcd33ec37faf06fc268d16e986
ldarcy_laptop <ldarcy@LDLenovoYoga.localdomain>
2025-10-25 17:39:43 +1100

    more styling + bg image

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,4 +1,8 @@
 module.exports = function (eleventyConfig) {
  eleventyConfig.addPassthroughCopy("./assets/css/");
  eleventyConfig.addWatchTarget("./assets/css/");
+
+ eleventyConfig.addPassthroughCopy("./assets/images/");
+ eleventyConfig.addWatchTarget("./assets/images/");
+
 };
